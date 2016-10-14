ee68f50a733b6d91de2205b5156452b45cb668c7
Ahmet Kocabıyık <ahmetkocabiyik@gmail.com>
2016-10-14 22:05:31 +0300

    proje tamamlandi

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -15,7 +15,7 @@ class KategoriController extends Controller
     {
         $kategori = Kategori::where("slug",$slug)->first();
 
-        $makaleler = Makale::where("kategori_id",$kategori->id)->where("durum",1)->paginate(10);
+        $makaleler = Makale::where("kategori_id",$kategori->id)->where("durum",1)->orderBy("created_at","desc")->paginate(10);
 
         return view("kategori",compact('kategori','makaleler'));
 
